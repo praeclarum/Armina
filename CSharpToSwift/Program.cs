18b524eec9471edc47cbd82473d3f6f3403d69f1
Frank A. Krueger <fak@praeclarum.org>
2022-06-22 15:41:01 -0700

    Create CSharpToSwift

---
+++ CSharpToSwift/Program.cs
@@ -0,0 +1,10 @@
+﻿using CSharpToSwift;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string projectFilePath = "/Users/fak/Dropbox/Projects/Circuit/CircuitMauiLib/CircuitMauiLib.csproj";
+
+
+var transpiler = new Transpiler(projectFilePath);
+await transpiler.TranspileAsync();
